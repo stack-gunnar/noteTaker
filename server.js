7beea336b43b00653ef5e2f39f83d118c79f31bd
William Judd <juddwilliam13@gmail.com>
2021-04-08 22:05:57 -0500

    having trouble loading css properly will advise

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const uuid = require("uuid");
 
 const PORT = process.env.PORT || 3000;
 
+let Notes = require("./Develop/db/db.json");
 
-app.use(express.static("public"));
+app.use(express.static( "public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,58 +18,50 @@ app.use(express.urlencoded({ extended: true }));
 //Routes to HTML
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
 
 
 
 
-app.get("/api/notes", (req, res) => {S
-  fs.readfile(path.join(__dirname, "./db/db.json"), (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    res.json(notes);
-  })
+app.get("/api/notes", (req, res) => {
+  res.json(Notes);
 });
+  
 
-
-app.post("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-    if(err) throw err;
     
-    const notes = JSON.parse(data);
-    const newNote = req.body;
-    newNote.id = uuid.v4();
-    notes.push(newNote);
-
-    const createNote = Json.stringify(notes);
-    fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) => {
-      if (err) throw err;
-    })
-    res.join(newNote);
-  });
+    
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id =uuid();
+  Notes.push(newNote);
+  res.json(newNote);
+  fs.writeFile("db.db.json", JSON.stringify(Notes), err => { if (err) { throw err;} return true;});
 });
 
-//Delete Saved Notes
+
+
+
+
+
 app.delete("/api/notes/:id", (req, res) => {
-  const noteID = req.params.id;
-  fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data); 
-    const notesArray = notes.filter(item => {
-      return item.id !== noteID
-    });
-
-    fs.writeFile("./db/db.json", JSON.stringify(notesArray), (err, data) => {
-      if (err) throw err;
-      res.json(notesArray)
-    });
-  });
+  let id = req.params.id;
+  for(var i = 0; 1 < Notes.length; i++) {
+    if (id === Notes[i].id) {
+      Notes.splice(i, 1);
+      fs.writeFile("db/db.json", JSON.stringify(Notes), err => {if (err){throw err;} return true; });
+    }
+  };
+  res.json(Notes);
 });
+
+
+
+
 
 
 app.listen(PORT, () => {
